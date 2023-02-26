9776e248a9f6e6e3076b521aa97af4e5cb38303e
dipeshgiri <dipeshgiri618@gmail.com>
2023-02-26 14:47:59 +0545

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel="stylesheet" href="app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="posts/first-post">My First Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt porro minima eius, voluptate minus voluptatum illum nobis temporibus cumque quidem aliquam at dolorem officia placeat nihil eum ab quaerat maxime.</p>
+    </article>
+
+
+    <article>
+        <h1><a href="posts/second-post">My Second Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt porro minima eius, voluptate minus voluptatum illum nobis temporibus cumque quidem aliquam at dolorem officia placeat nihil eum ab quaerat maxime.</p>
+    </article>
+
+    <article>
+        <h1><a href="posts/third-post">My Third Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt porro minima eius, voluptate minus voluptatum illum nobis temporibus cumque quidem aliquam at dolorem officia placeat nihil eum ab quaerat maxime.</p>
+    </article>
+
+
+</body>
+</html>
